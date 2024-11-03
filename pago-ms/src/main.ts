696a40b2650803a5eb87b229cfff6e81d74be1ca
Rodrigo Pérez <rodrigop_23@outlook.es>
2024-11-03 16:44:09 -0500

    chore: add envs

--- pago-ms/src/main.ts
+++ pago-ms/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config/envs';
 
 async function bootstrap() {
+  const logger = new Logger('Pago-Microservice');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.port);
+
+  logger.log(`Products Microservice running on port ${envs.port}`);
 }
 bootstrap();
